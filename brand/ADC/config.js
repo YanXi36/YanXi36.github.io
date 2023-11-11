cbac7e02c69fd679b34cf54ba38a38d033c3fc35
DESKTOP-0DG591Q\mayta <mayta51654944@gmail.com>
2023-11-11 16:07:10 +0800

    optimizer

---
+++ brand/ADC/config.js
@@ -0,0 +1,158 @@
+//    Developer: Yantong Wu
+//    File Purpose: The config of AKATO series
+//    Update Date : 2023/11/11
+
+var productConfigs = {
+    Gao_jia: {
+        imageInfo: [
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
+        ],
+        imagePath: "img/Gao_jia/",
+    },
+    Gradient: {
+        imageInfo: [
+            { fileName: "66JP360.jpg", displayText: "66JP360" },
+            { fileName: "66JP361.jpg", displayText: "66JP361" },
+        ],
+        imagePath: "img/Gradient/",
+    },
+    Goy: {
+        imageInfo: [
+            { fileName: "931JP420.jpg", displayText: "931JP420" },
+            { fileName: "931JP421.jpg", displayText: "931JP421" },
+        ],
+        imagePath: "img/Goy/",
+    },
+    Gao_ling: {
+        imageInfo: [
+            { fileName: "45JP450.jpg", displayText: "47JP450 (亮面)" },
+            { fileName: "45JP451.jpg", displayText: "45JP451" },
+        ],
+        imagePath: "img/Gao_ling/",
+    },
+    Gao_porcelain: {
+        imageInfo: [
+            { fileName: "pro_e_1_03.jpg", displayText: "45JP061" },
+        ],
+        imagePath: "img/Gao_porcelain/",
+    },
+    Gao_xiang: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Gao_xiang/",
+    },
+    Gose: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Gose/",
+    },
+    Gao_ya: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Gao_ya/",
+    },
+    Gao_ge: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Gao_ge/",
+    },
+    Dian_jing: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Dian_jing/",
+    },
+    Gao_zheng01: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Gao_zheng01/",
+    },
+    Gao_zheng23: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Gao_zheng23/",
+    },
+    Gao_cheng: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Gao_cheng/",
+    },
+    Gao_fang: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Gao_fang/",
+    },
+    Gao_fu: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Gao_fu/",
+    },
+    Gao_so: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Gao_so/",
+    },
+    Gao_ping: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Gao_ping/",
+    },
+    Gao_guang: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Gao_guang/",
+    },
+    Gao_wang: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Gao_wang/",
+    },
+    Gao_yan: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Gao_yan/",
+    },
+    Gao_pin: {
+        imageInfo: [
+            { fileName: "221JP435.jpg", displayText: "221JP435" },
+            { fileName: "221JP436.jpg", displayText: "221JP436" },
+        ],
+        imagePath: "img/Gao_pin/",
+    },
+    // Add more series configurations as needed
+};
+
+// Example of accessing the configuration for Series1
+// var series1Config = productConfigs.Marrow;
+// var series1ImageInfo = series1Config.imageInfo;
+// var series1ImagePath = series1Config.imagePath;
