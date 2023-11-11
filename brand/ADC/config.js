ced9246819c664a4b002c95e8f13f51b09c76edf
DESKTOP-0DG591Q\mayta <mayta51654944@gmail.com>
2023-11-11 16:31:34 +0800

    optimizer

--- brand/ADC/config.js
+++ brand/ADC/config.js
@@ -12,140 +12,141 @@ var productConfigs = {
     },
     Gradient: {
         imageInfo: [
-            { fileName: "66JP360.jpg", displayText: "66JP360" },
-            { fileName: "66JP361.jpg", displayText: "66JP361" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gradient/",
     },
     Goy: {
         imageInfo: [
-            { fileName: "931JP420.jpg", displayText: "931JP420" },
-            { fileName: "931JP421.jpg", displayText: "931JP421" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Goy/",
     },
     Gao_ling: {
         imageInfo: [
-            { fileName: "45JP450.jpg", displayText: "47JP450 (亮面)" },
-            { fileName: "45JP451.jpg", displayText: "45JP451" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_ling/",
     },
     Gao_porcelain: {
         imageInfo: [
-            { fileName: "pro_e_1_03.jpg", displayText: "45JP061" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_porcelain/",
     },
     Gao_xiang: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_xiang/",
     },
     Gose: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gose/",
     },
     Gao_ya: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_ya/",
     },
     Gao_ge: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_ge/",
     },
     Dian_jing: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Dian_jing/",
     },
     Gao_zheng01: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_zheng01/",
     },
     Gao_zheng23: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_zheng23/",
     },
     Gao_cheng: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_cheng/",
     },
     Gao_fang: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_fang/",
     },
     Gao_fu: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_fu/",
     },
     Gao_so: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_so/",
     },
     Gao_ping: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_ping/",
     },
     Gao_guang: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_guang/",
     },
     Gao_wang: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_wang/",
     },
     Gao_yan: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_yan/",
     },
     Gao_pin: {
         imageInfo: [
-            { fileName: "221JP435.jpg", displayText: "221JP435" },
-            { fileName: "221JP436.jpg", displayText: "221JP436" },
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
         ],
         imagePath: "img/Gao_pin/",
     },
