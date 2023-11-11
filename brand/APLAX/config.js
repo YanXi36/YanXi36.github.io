2d88d8cdbe4130ec13e676ad580a26fefc7e7fcb
DESKTOP-0DG591Q\mayta <mayta51654944@gmail.com>
2023-11-11 11:52:41 +0800

    Optimizer

---
+++ brand/APLAX/config.js
@@ -0,0 +1,46 @@
+var productConfigs = {
+    Marrow: {
+        imageInfo: [
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
+            { fileName: "23JP241.jpg", displayText: "23JP241" },
+            { fileName: "23JP242.jpg", displayText: "23JP242" },
+            { fileName: "23JP245.jpg", displayText: "23JP245" },
+            { fileName: "23JP246.jpg", displayText: "23JP246" },
+            { fileName: "23JP247.jpg", displayText: "23JP247" },
+            { fileName: "23JP249.jpg", displayText: "23JP249" },
+            { fileName: "373JP260.jpg", displayText: "373JP260" },
+            { fileName: "373JP262.jpg", displayText: "373JP262" },
+            { fileName: "373JP265.jpg", displayText: "373JP265" },
+            { fileName: "373JP266.jpg", displayText: "373JP266" },
+            { fileName: "373JP302.jpg", displayText: "373JP302" },
+            { fileName: "373JP305.jpg", displayText: "373JP305" },
+        ],
+        imagePath: "img/Marrow/",
+    },
+    Marrow1: {
+        imageInfo: [
+            { fileName: "23JP200.jpg", displayText: "23JP200" },
+            { fileName: "23JP201.jpg", displayText: "23JP201" },
+            { fileName: "23JP241.jpg", displayText: "23JP241" },
+            { fileName: "23JP242.jpg", displayText: "23JP242" },
+            { fileName: "23JP245.jpg", displayText: "23JP245" },
+            { fileName: "23JP246.jpg", displayText: "23JP246" },
+            { fileName: "23JP247.jpg", displayText: "23JP247" },
+            { fileName: "23JP249.jpg", displayText: "23JP249" },
+            { fileName: "373JP260.jpg", displayText: "373JP260" },
+            { fileName: "373JP262.jpg", displayText: "373JP262" },
+            { fileName: "373JP265.jpg", displayText: "373JP265" },
+            { fileName: "373JP266.jpg", displayText: "373JP266" },
+            { fileName: "373JP302.jpg", displayText: "373JP302" },
+            { fileName: "373JP305.jpg", displayText: "373JP305" },
+        ],
+        imagePath: "img/Marrow/",
+    },
+    // Add more series configurations as needed
+};
+
+// Example of accessing the configuration for Series1
+// var series1Config = productConfigs.Marrow;
+// var series1ImageInfo = series1Config.imageInfo;
+// var series1ImagePath = series1Config.imagePath;
